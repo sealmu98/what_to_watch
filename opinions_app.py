89f53a0e86c6e417615163f11353299bd492c51b
sealmu <seal.mu98@gmail.com>
2025-06-01 23:22:03 +0300

    Спринт 21/26 → Тема 2/4: Flask на практике → Урок 6/15: Доработка шаблонов

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from datetime import datetime
+
+# Импортируется функция для выбора случайного значения:
+from random import randrange
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    # ID — целое число, первичный ключ:
+    id = db.Column(db.Integer, primary_key=True)
+    # Название фильма — строка длиной 128 символов, не может быть пустым:
+    title = db.Column(db.String(128), nullable=False)
+    # Мнение о фильме — большая строка, не может быть пустым,
+    # должно быть уникальным:
+    text = db.Column(db.Text, unique=True, nullable=False)
+    # Ссылка на сторонний источник — строка длиной 256 символов:
+    source = db.Column(db.String(256))
+    # Дата и время — текущее время,
+    # по этому столбцу база данных будет проиндексирована:
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных записей нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    # Тут подключаем шаблон opinion.html:
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add')
+def add_opinion_view():
+    # Тут подключаем шаблон add_opinion.html:
+    return render_template('add_opinion.html')
+
 
 if __name__ == '__main__':
     app.run()
