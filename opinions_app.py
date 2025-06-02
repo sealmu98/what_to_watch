c0fc7a45c28a63ab1474802a0e033c0968bff236
sealmu <seal.mu98@gmail.com>
2025-06-02 21:24:03 +0300

    Спринт 21/28 → Тема 2/4: Flask на практике → Урок 12/15: Кастомные страницы ошибок

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -54,32 +54,33 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        # Если в базе пусто, при запросе к главной странице
+        # пользователь увидит ошибку 500:
+        abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    # Тут подключаем шаблон opinion.html:
     return render_template('opinion.html', opinion=opinion)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
     form = OpinionForm()
-    # Если ошибок не возникло...
     if form.validate_on_submit():
-        # ...то нужно создать новый экземпляр класса Opinion:
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # ...вызвать функцию flash и передать соответствующее сообщение:
+            flash('Такое мнение уже было оставлено ранее!')
+            # Вернуть пользователя на страницу «Добавить новое мнение»:
+            return render_template('add_opinion.html', form=form)
         opinion = Opinion(
-            # И передать в него данные, полученные из формы:
             title=form.title.data,
-            text=form.text.data,
+            text=text,
             source=form.source.data
         )
-        # Затем добавить его в сессию работы с базой данных:
         db.session.add(opinion)
-        # И зафиксировать изменения:
         db.session.commit()
-        # Затем переадресовать пользователя на страницу добавленного мнения:
         return redirect(url_for('opinion_view', id=opinion.id))
-    # Если валидация не пройдена — просто отрисовать страницу с формой:
     return render_template('add_opinion.html', form=form)
 
 
@@ -91,6 +92,28 @@ def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     # ...и передать его в шаблон (шаблон тот же, что и для главной страницы):
     return render_template('opinion.html', opinion=opinion)
+
+
+# Тут декорируется обработчик и указывается код нужной ошибки:
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере.
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, не зафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница,
+    # сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
