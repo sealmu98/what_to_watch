c20f51eca19266cce40b16ff99ea4f3d5ac89fa9
sealmu <seal.mu98@gmail.com>
2025-06-03 19:15:11 +0300

    Спринт 21/26 → Тема 2/4: Flask на практике → Урок 14/15: Пользовательские команды Flask

--- migrations/versions/85753c425355_added_added_by_field.py
+++ migrations/versions/85753c425355_added_added_by_field.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2025-06-03 19:02:19.677950
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '85753c425355'
--- opinions_app.py
+++ opinions_app.py
@@ -1,14 +1,14 @@
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
-
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -16,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SECRET_KEY'] = '1234'
 
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 
@@ -97,6 +98,13 @@ def opinion_view(id):
     return render_template('opinion.html', opinion=opinion)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
+
+
 # Тут декорируется обработчик и указывается код нужной ошибки:
 @app.errorhandler(500)
 def internal_error(error):
@@ -112,11 +120,25 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    # При ошибке 404 в качестве ответа вернётся страница, созданная
-    # на основе шаблона 404.html, и код HTTP-ответа 404:
-    return render_template('404.html'), 404
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открываем файл:
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создаём итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла:
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавляем счётчик:
+        counter = 0
+        for row in reader:
+            # Распакованный словарь используем
+            # для создания экземпляра модели Opinion:
+            opinion = Opinion(**row)
+            # Добавляем объект в сессию и коммитим:
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
